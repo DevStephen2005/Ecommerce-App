8f46c098cba9a7157081d0765191b2bfaabadbd7
stephen5122003@gmail.com <stephen5122003@gmail.com>
2024-03-29 13:14:22 +0530

    All files are uploaded successfully

---
+++ app2.js
@@ -0,0 +1,11 @@
+// Select sidenavbar1
+sideNavbar1=document.querySelector(".sidenavbar1")
+
+function showNav()
+{
+    sideNavbar1.style.left="0"
+}
+function closeNav()
+{
+    sideNavbar1.style.left="-60%"
+}
